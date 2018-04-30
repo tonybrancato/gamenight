88b8e46b8b3d30c85c49ec78fcdbc028201e3647
Tony Brancato <tonybrancato1@gmail.com>
2018-04-30 18:32:37 -0400

    test game data

--- src/reducers/games-reducer.js
+++ src/reducers/games-reducer.js
@@ -6,13 +6,20 @@ const initialState = {
       name: "Splendor",
       type: "Board Game",
       players: "2 - 4",
-      rating: "3"
+      rating: "3",
+      games: [
+        {date: '03/28/2018', win: 1, players: ['Mom', 'Kayla', 'Anthony']},
+        {date: '04/28/2018', win: 1, players: ['Mom', 'Kayla', 'Anthony']},
+        {date: '04/20/2018', win: 0, players: ['Mom', 'Kayla', 'Anthony']},
+        {date: '04/04/2018', win: 1, players: ['Mom', 'Kayla', 'Anthony']},
+        {date: '04/15/2018', win: 1, players: ['Mom', 'Kayla', 'Anthony']},
+      ]
     },
     {
       name: "Star Wars Rebellion",
       type: "Board Game",
       players: "2 - 4",
-      rating: "5"
+      rating: "5",
     }
   ],
 };
